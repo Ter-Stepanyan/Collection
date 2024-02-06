a86761e75ed5a45637a123016f9f8561f5a8fe1b
Ter-Stepanyan <artak.ter-stepanyan@team.gg>
2024-02-06 19:21:06 +0400

    Initial Commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by Artak Ter-Stepanyan on 06.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
